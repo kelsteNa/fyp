58142ba9c4eef7e97507b335c05584c353dbae26
Thomas Roberts <thomas@thomas.lancs.ac.uk>
2013-02-25 12:58:57 +0000

    making the first push to the repo

---
+++ build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['scratch/ns3.16-scratch-simulator-debug', 'scratch/subdir/ns3.16-subdir-debug', 'scratch/ns3.16-new-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
